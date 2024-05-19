d5b742e34bb97576361946516660b48448fcce10
Arthur Caumartin <artcaumart@gmail.com>
2024-05-19 02:07:34 +0200

    update

--- Assets/[Project]/Scripts/PlayerMovement.cs
+++ Assets/[Project]/Scripts/PlayerMovement.cs
@@ -6,36 +6,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Type : ")]
-    [SerializeField] private bool _lerp = false;
-    [SerializeField] private bool _damp = false;
-    [Space]
     [SerializeField] private float _speed = 0.1f;
     private bool _isScreenClicked = false;
     private Vector2 _worldInputVector;
     private Camera mainCam;
     private Vector2 dampVelocity;
+    private Vector3 _lastFramePosition;
 
     void Start()
     {
         mainCam = Camera.main;
+        _lastFramePosition = transform.localPosition;
     }
 
     void Update()
     {
+        transform.forward = (transform.localPosition - _lastFramePosition).normalized;
+
         Vector2 moveTarget = Vector2.zero;
         if (_isScreenClicked)
-        {
-            print("Move");
-            if (_lerp)
-                moveTarget = Vector2.Lerp(transform.position, _worldInputVector, Time.deltaTime);
-            if (_damp)
-                moveTarget = Vector2.SmoothDamp(transform.position, _worldInputVector, ref dampVelocity, _speed, Mathf.Infinity, Time.deltaTime);
-        }
+            moveTarget = Vector2.SmoothDamp(transform.position, _worldInputVector, ref dampVelocity, _speed, Mathf.Infinity, Time.deltaTime);
         else
             moveTarget = transform.position;
 
         transform.position = moveTarget;
+
+        _lastFramePosition = transform.localPosition;
     }
 
     private void OnPointerClic(InputValue value)
@@ -46,22 +42,5 @@ public class PlayerMovement : MonoBehaviour
     private void OnPointerMove(InputValue value)
     {
         _worldInputVector = mainCam.ScreenToWorldPoint(value.Get<Vector2>());
-    }
-
-    private void OnGUI()
-    {
-        _speed = GUI.HorizontalSlider(new Rect(350, 10, 600, 10), _speed, 0.01f, 5);
-
-        if (GUI.Button(new Rect(10, 10, 150, 100), "Lerp"))
-        {
-            _damp = false;
-            _lerp = true;
-        }
-
-        if (GUI.Button(new Rect(170, 10, 150, 100), "Damp"))
-        {
-            _damp = true;
-            _lerp = false;
-        }
     }
 }
