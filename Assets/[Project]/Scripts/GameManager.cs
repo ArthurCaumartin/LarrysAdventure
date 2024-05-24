d6a01196add9fa38def8004d5f6ae49c6909da96
Arthur Caumartin <artcaumart@gmail.com>
2024-05-24 14:07:42 +0200

    git fix ?

---
+++ Assets/[Project]/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadDevScene()
+    {
+        Time.timeScale = 0;
+        Transitioner.instance.LarryTransition(() =>
+        {
+            SceneManager.LoadScene("SceneDev");
+        }, () => Time.timeScale = 1);
+    }
+
+    public void ResetCurrentLevel()
+    {
+        Time.timeScale = 0;
+        Transitioner.instance.ResetLevelTransition(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }, () => Time.timeScale = 1);
+    }
+}
